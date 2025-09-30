bfe3e44ec0fe207d529fb84925bb055e5f21e381
Hironori Wada <hironori104@gmail.com>
2025-09-30 10:53:41 +0900

    add toggle debug command

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,20 @@
 import * as vscode from "vscode";
-import { DebugSymbolsCommand } from "./commands/debug-symbols";
-import { JumpToSiblingSymbolCommand } from "./commands/jump-to-next-sibling-symbol";
-import { commandTimer } from "./command/middleware";
-import { JumpToNextSymbolCommand } from "./commands/jump-to-next-symbol";
-import { JumpToFirstSiblingSymbolCommand } from "./commands/jump-to-first-sibling";
 import { ExtensionMode } from "vscode";
+import { commandTimer } from "./command/middleware";
+import { DebugSymbolsCommand } from "./commands/debug-symbols";
+import { JumpToFirstSiblingSymbolCommand } from "./commands/jump-to-first-sibling";
+import { JumpToSiblingSymbolCommand } from "./commands/jump-to-next-sibling-symbol";
+import { JumpToNextSymbolCommand } from "./commands/jump-to-next-symbol";
+import { ToggleDebugCommand } from "./commands/toggle-debug";
 
 export function activate(context: vscode.ExtensionContext) {
   const middleware =
     context.extensionMode === ExtensionMode.Production ? [commandTimer()] : [];
 
   const commands = [
+    new ToggleDebugCommand(context, "cursor-ninja.toggleDebug", [
+      commandTimer(),
+    ]),
     new DebugSymbolsCommand(context, "cursor-ninja.debugSymbols", [
       commandTimer(),
     ]),
